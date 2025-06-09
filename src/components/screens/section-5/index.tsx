9049bceb2c69e467deb070aede02ec1cf424322f
Joao Pedro A Seco <dev.jpedro@gmail.com>
2025-06-08 23:52:01 -0300

    chore: adding form section

---
+++ src/components/screens/section-5/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import Image from "next/image";
+
+export const Section5 = () => {
+  const { is768 } = useMediaQuery();
+
+  return (
+    <section
+      id="contact"
+      className="flex flex-col w-full h-fit min-h-screen max-h-[100vh] justify-center items-center relative bg-[#03042C]"
+      style={{
+        backgroundImage: `url(/sections/4/${is768 ? "mobile/" : ""}bg.png)`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="flex flex-col justify-center items-center absolute z-1 w-full h-full max-w-[68.80%] max-[769px]:max-w-[100%] mx-auto">
+        <Image
+          src={"/sections/4/form-headline.svg"}
+          width={562}
+          height={144}
+          alt="headline"
+        />
+      </div>
+    </section>
+  );
+};
